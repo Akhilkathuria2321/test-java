d1212b62a98f963b7dc6758b05d9365d10ec774a
Akki <Akki@Akki-PC>
2017-11-04 16:37:19 +0530

    changesccccccccccc

--- Example.java
+++ Example.java
@@ -18,7 +18,7 @@ public class Example {
  public static void main(final String[] args) {
 
 
-	 // Thats a change - new
+	 // Thats a change - new one change
      System.out.println("Findbugs Sample 001 for BC_IMPOSSIBLE_CAST");
 
      // WRONG
